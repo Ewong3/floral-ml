0d1f339afe76731d1d011a50cde20a448771240a
Ewong3 <Ewong3@users.noreply.github.com>
2020-03-28 21:33:03 -0600

    tensorflow model

---
+++ tensorflow/data.js
@@ -0,0 +1,86 @@
+const tf = require('@tensorflow/tfjs');
+const { Image, createCanvas } = require('canvas');
+const fs = require('fs');
+const path = require('path');
+
+const TRAINING_IMAGES_PATH = './tensorflow/data/training';
+const TESTING_IMAGES_PATH = './tensorflow/data/testing';
+
+const DAISY = 'daisy';
+const DANDELION = 'dandelion';
+const ROSE = 'rose';
+const SUNFLOWER = 'sunflower';
+const TULIP = 'tulip';
+
+const FLOWERS = [DAISY, DANDELION];//, ROSE, SUNFLOWER, TULIP];
+
+const loadImageFromPath = (filePath) => {
+    const canvas = createCanvas(800, 600);
+    const ctx = canvas.getContext('2d');
+
+    var img = new Image();
+    img.onload = () => ctx.drawImage(img, 0, 0);
+    img.onerror = err => { throw err };
+    img.src = filePath;
+
+    return canvas;
+}
+
+const loadImages = (prefixPath) => {
+    const images = [];
+    const labels = [];
+
+    FLOWERS.forEach((flower, index) => {
+        const flowerPath = path.join(prefixPath, flower);
+        // console.log(flowerPath);
+
+        var files = fs.readdirSync(flowerPath);
+        
+        files.forEach((file) => {
+            var filePath = path.join(flowerPath, file);
+
+            const image = loadImageFromPath(filePath);
+
+            var imageTensor = tf.browser.fromPixels(image)
+                .resizeNearestNeighbor([96,96])
+                .toFloat()
+                .div(tf.scalar(255.0))
+                .expandDims();
+
+            images.push(imageTensor);
+            labels.push(index);
+        });
+    });
+    // console.log(images);
+    // console.log(labels);
+    
+    return [images, labels];
+}
+
+class FlowerDataset {
+    constructor() {
+        this.trainData = [];
+        this.testData = [];
+    }
+  
+    loadData() {
+      this.trainData = loadImages(TRAINING_IMAGES_PATH);
+      this.testData = loadImages(TESTING_IMAGES_PATH);
+    }
+  
+    getTrainData() {
+      return {
+        images: tf.concat(this.trainData[0]),
+        labels: tf.oneHot(tf.tensor1d(this.trainData[1], 'int32'), 2).toFloat(),
+      }
+    }
+  
+    getTestData() {
+      return {
+        images: tf.concat(this.testData[0]),
+        labels: tf.oneHot(tf.tensor1d(this.testData[1], 'int32'), 2).toFloat()
+      }
+    }
+  }
+
+module.exports = new FlowerDataset();
---
+++ tensorflow/main.js
@@ -0,0 +1,50 @@
+const tf = require('@tensorflow/tfjs');
+
+const data = require('./data');
+const model = require('./model');
+
+async function run(epochs, batchSize, modelSavePath) {
+    data.loadData();
+  
+    const {images: trainImages, labels: trainLabels} = data.getTrainData();
+    console.log("Training Images (Shape): " + trainImages.shape);
+    console.log("Training Labels (Shape): " + trainLabels.shape);
+    
+    console.log('SUMMARY');
+    model.summary();
+
+    const validationSplit = 0.15;
+    
+    await model.fit(trainImages, trainLabels, {
+        epochs,
+        batchSize,
+        validationSplit,
+        callbacks: {
+            onTrainBegin: () => console.log('train begin'),
+            onTrainEnd: () => console.log('train end'),
+            onEpochBegin: () => console.log('epoch begin'),
+            onEpochEnd: () => console.log('epoch end'),
+            onBatchBegin: () => console.log('batch begin'),
+            onBatchEnd: () => console.log('batch end'),
+            onYield: () => console.log('yield'),
+
+        },
+    });
+
+    console.log('FIT');
+  
+    const {images: testImages, labels: testLabels} = data.getTestData();
+    const evalOutput = model.evaluate(testImages, testLabels);
+  
+    console.log(
+        `\nEvaluation result:\n` +
+        `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; `+
+        `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
+  
+    if (modelSavePath != null) {
+        await model.save(`file://${modelSavePath}`);
+        console.log(`Saved model to path: ${modelSavePath}`);
+    }
+  }
+  
+  run(5, 5, './tensorflow/model');
