e5f53c2078819e733021ae53037d70a0d342aee1
Ewong3 <Ewong3@users.noreply.github.com>
2020-04-27 15:09:20 -0600

    Added Floral-ML home page description and links to external sources

---
+++ src/constants/sources.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+
+const sources = [{
+    text: 'Floral-ML Code',
+    url: 'https://github.com/Ewong3/floral-ml',
+    icon: <SocialIcon network="github" bgColor="#000000"/>,
+}, {
+    text: 'ReactJS',
+    url: 'https://reactjs.org/',
+    icon: <SocialIcon network="github" bgColor="#000000"/>,
+}, {
+    text: 'TensorflowJS',
+    url: 'https://www.tensorflow.org/js',
+    icon: <SocialIcon network="github" bgColor="#000000"/>,
+}, {
+    text: 'United States Department of Agriculture',
+    url: 'https://www.usda.gov/',
+    icon: <SocialIcon network="github" bgColor="#000000"/>,
+}, {
+    text: 'Wikipedia',
+    url: 'https://www.wikipedia.org/',
+    icon: <SocialIcon network="github" bgColor="#000000"/>,
+}];
+
+export default sources;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,23 +1,49 @@
 import React, { PureComponent } from 'react'
-import { Container, Button } from '@material-ui/core'
-import { SocialIcon } from 'react-social-icons';
+import { Container, Button, Grid } from '@material-ui/core'
+
+import sources from '../../constants/sources';
 
 class Home extends PureComponent {
-    
-    render() {
+
+    renderIntro = () => {
         return (
-            <Container>
+            <Grid item xs={8}>
                 <h1>Welcome to Floral-ML!</h1>
                 <p>Floral-ML is an image recognition website which identifies pictures of flowers. Floral-ML was developed using ReactJS and TensorflowJS.</p>
                 <p>Click Discover in the menu to start uploading and identifying floral images!</p>
                 <p>You can also check out the Encyclopedia to browse a database of numerous plants and flower. The source of data is from the United States Department of Agriculture and Wikipedia.</p>
+            </Grid>
+        );
+    }
 
+    renderLinkButton = (source) => {
+        return (
+            <Grid item xs={12}>
                 <Button
-                    onClick={() => window.open('https://github.com/Ewong3/floral-ml')}
-                    startIcon={<SocialIcon network="github"/>}
+                    onClick={() => window.open(source.url)}
+                    startIcon={source.icon}
                 >
-                    View the source code here.
+                    { source.text }
                 </Button>
+            </Grid>
+        );
+    }
+
+    renderLinks = () => {
+        return (
+            <Grid item xs={4}>
+                { sources.map(this.renderLinkButton) }
+            </Grid>
+        );
+    }
+
+    render() {
+        return (
+            <Container>
+                <Grid container spacing={6}>
+                    { this.renderIntro() }
+                    { this.renderLinks() }
+                </Grid>
             </Container>
         );
     }
